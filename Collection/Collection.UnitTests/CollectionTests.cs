ce9ca2b1e25e8aab8f23437a034c3aa29ff0d8d3
Elitsa-Georgieva <itsa.georgieva@gmail.com>
2023-02-04 15:50:10 +0200

    DDT_Collection_NUnitTests_GetByInvalidIndex Added

--- Collection/Collection.UnitTests/CollectionTests.cs
+++ Collection/Collection.UnitTests/CollectionTests.cs
@@ -4,7 +4,7 @@ namespace Collection.UnitTests
 {
     public class CollectionTests
     {
-        
+
 
         [TestCase("Peter,Maria,Ivan", 0, "Peter")]
         public void Test_Collection_GetByValidIndex(string data, int index, string expected)
@@ -14,6 +14,19 @@ namespace Collection.UnitTests
             var actual = coll[index];
 
             Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("", 0)]
+        [TestCase("Peter",-1)]
+        [TestCase("Peter", 1)]
+        [TestCase("Peter,Maria,Ivan", -1)]
+        [TestCase("Peter,Maria,Ivan", 3)]
+        [TestCase("Peter,Maria,Ivan", 150)]
+        public void Test_Collection_GetByInvalidIndex(string data, int index)
+        {
+            var coll = new Collection<string>(data.Split(",", StringSplitOptions.RemoveEmptyEntries));
+
+            Assert.That(() => coll[index], Throws.TypeOf<ArgumentOutOfRangeException>());
         }
     }
 }
